7b5907db817547c499b7b7d02c56e81469af1d38
Hamid Reza Sepehr <hamidsepehr4@gmail.com>
2021-03-31 13:20:48 +0430

    Update Veronica

--- veronica/js/preloader.js
+++ veronica/js/preloader.js
@@ -32,19 +32,20 @@
 	        preloadWrapper.appendChild(progressNode);
 	        preloadWrapper.appendChild(percentNode);
 
-	        body.appendChild(preloadWrapper);
+	        body.prepend(preloadWrapper);
 
 	        let pageStatus = null;
 		    let progress = null;
+		    let animationInterval = 33;
 		    
 		   
 		    
-
+		    updateProgress();
 		   
 		    function updateProgress(){
 		        if(pageStatus == "complete"){
 		            percentNode.innerHTML = 100;
-		            clearInterval(updateInterval);
+		            progressNode.style.width = "100%";
 		            setTimeout(function(){
 		                body.classList.remove('loading');
 		                body.classList.add('loaded');
@@ -58,32 +59,43 @@
 		            progress = progress + 1;
 		            if(progress >= 0 && progress <= 30){
 		                percentNode.innerHTML = progress;
+		                progressNode.style.width = progress + "%";
+		                animationInterval += 1;
 		            }
 		            else if(progress > 30 && progress <= 60){
 		                percentNode.innerHTML = progress;
+		                progressNode.style.width = progress + "%";
+		                animationInterval += 2;
 		            }
 		            else if(progress > 60 && progress <= 80){
 		                percentNode.innerHTML = progress;
+		                progressNode.style.width = progress + "%";
+		                animationInterval += 3;
 		            }
 		            else if(progress > 80 && progress <= 90){
 		                percentNode.innerHTML = progress;
+		                progressNode.style.width = progress + "%";
+		                animationInterval += 4;
 		            }
 		            else if(progress > 90 && progress <= 95){
 		                percentNode.innerHTML = progress;
+		                progressNode.style.width = progress + "%";
+		                animationInterval += 80;
 		            }
 		            else if(progress > 95 && progress <= 99){
 		                percentNode.innerHTML = progress;
+		                progressNode.style.width = progress + "%";
+		                animationInterval += 150;
 		            }
 		            else if(progress >= 100){
 		                percentNode.innerHTML = 99;
+		                progressNode.style.width = progress + "%";
 		            }
+		            setTimeout(updateProgress, animationInterval);
 		        }
 		        console.log(progress);
 		    }
 		   
-		    let updateInterval = setInterval(()=>{
-		    	updateProgress();
-		    }, 50);
 
 		    window.document.addEventListener("readystatechange", function(){
 		        if(document.readyState == "complete"){
